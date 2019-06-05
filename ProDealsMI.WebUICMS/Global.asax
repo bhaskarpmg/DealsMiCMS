54d8618f10102aac60ab885cff0d5668857d6e48
dsantoshidivya@gmail.com <dsantoshidivya@gmail.com>
2019-06-05 05:58:35 -0700

    Web dll

---
+++ ProDealsMI.WebUICMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProDealsMI.WebUICMS.MvcApplication" Language="C#" %>
---
+++ ProDealsMI.WebUICMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProDealsMI.WebUICMS.Startup))]
+namespace ProDealsMI.WebUICMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
